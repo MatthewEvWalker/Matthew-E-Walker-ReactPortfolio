664c87c28b1658fa0f76f80d4de91a7573585fbc
Matthew Walker <matthewevwalker@gmail.com>
2023-04-26 01:43:56 -0500

    added the home route and the html with it, Home.scss has been editted and now I need to finish up the Home layout.

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './Home.scss';
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-area">
+                <h1> Hi, I'm Matthew 
+                FullStack Developer
+                </h1>
+                <h2>
+                React Dev | Intermediate Python Programmer | Advanced Javascript Skills
+                </h2>
+                <Link to="/contact" className="flat-button">Contact</Link>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,7 @@ import './Sidebar.scss';
 import logoMW from '../../assets/images/MW-logo_thumbnail.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faDiscord, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => (
@@ -21,6 +22,33 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="$4d4d4e"/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://www.linkedin.com/in/matthew-ev-walker/'
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://github.com/MatthewEvWalker/'
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://github.com/MatthewEvWalker/'
+                >
+                    <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
