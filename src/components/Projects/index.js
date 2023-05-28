bb8bb61611a5eded652020b6aeb8f895ff363d17
Matthew Walker <matthewevwalker@gmail.com>
2023-05-28 02:08:39 -0500

    at a great stopping point, the grid is set up for the projects with working links, just need to add different images for them, change the links and also change the image for myself on the Home page. UPLOAD GOOGLE DRIVE PHOTOS

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,8 @@
 import './Projects.scss'
 import Letters from '../Letters'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
+import me1 from '../../assets/images/landscape_sun_field.jpg';
+import me2 from '../../assets/images/mount_river.avif';
 
 const Project = () => {
     const [letterClass, setLetterClass] = useState('text-animate') 
@@ -27,6 +29,44 @@ const Project = () => {
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet scelerisque risus quis auctor. Ut ac nisi quis ex rhoncus eleifend nec non tortor. Fusce porttitor a dui non maximus. Curabitur vehicula varius massa non blandit. Proin feugiat turpis sem, et consequat augue pretium quis. Aenean vitae bibendum lectus. Nullam vestibulum arcu vestibulum nisi volutpat, tristique ornare nibh tempus. In sed luctus quam, non dapibus est.
                 </p>
+            </div>
+
+            <div className="project-area">
+                <div className="project-1">
+                    {/* <h2>Project 1</h2> */}
+                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
+                        <img src={me1} height="300" width="425" />
+                    </a>
+
+
+                </div>
+
+                <div className="project-2" >
+                    {/* <h2>Project 1</h2> */}
+
+
+                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
+                        <img src={me1} height="300" width="425" />
+                    </a>
+
+                </div>
+
+                <div className="project-3">
+                    {/* <h2>Project 1</h2> */}
+                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
+                        <img src={me1} height="300" width="425" />
+                    </a>
+
+                </div>
+
+                <div className="project-4">
+                    {/* <h2>Project 1</h2> */}
+                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
+                        <img src={me1} height="300" width="425" />
+                    </a>
+
+
+                </div>
             </div>
         </div>
     )
