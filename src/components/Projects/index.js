0b7c025646f59355fb076acf1ba6e990ebb5e916
Matthew Walker <matthewevwalker@gmail.com>
2023-05-28 13:46:20 -0500

    changed the project photos, added the links and changed h1 bg color

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,10 @@ import Letters from '../Letters'
 import { useState, useEffect } from 'react'
 import me1 from '../../assets/images/landscape_sun_field.jpg';
 import me2 from '../../assets/images/mount_river.avif';
+import crypto from '../../assets/images/crypto-game.png';
+import slither from '../../assets/images/slither.png';
+import workDay from '../../assets/images/work-day-scheduler.png';
+import password from '../../assets/images/password-gen.png';
 
 const Project = () => {
     const [letterClass, setLetterClass] = useState('text-animate') 
@@ -34,8 +38,8 @@ const Project = () => {
             <div className="project-area">
                 <div className="project-1">
                     {/* <h2>Project 1</h2> */}
-                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
-                        <img src={me1} height="300" width="425" />
+                    <a target="_blank" href='https://matthewevwalker.github.io/whatacrypto-crypto-game/' >
+                        <img src={crypto} height="300" width="425" />
                     </a>
 
 
@@ -45,24 +49,24 @@ const Project = () => {
                     {/* <h2>Project 1</h2> */}
 
 
-                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
-                        <img src={me1} height="300" width="425" />
+                    <a target="_blank" href='https://github.com/MatthewEvWalker/Slither-AI' >
+                        <img src={slither} height="300" width="425" />
                     </a>
 
                 </div>
 
                 <div className="project-3">
                     {/* <h2>Project 1</h2> */}
-                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
-                        <img src={me1} height="300" width="425" />
+                    <a target="_blank" href='https://matthewevwalker.github.io/work-scheduler-5/' >
+                        <img src={workDay} height="300" width="425" />
                     </a>
 
                 </div>
 
                 <div className="project-4">
                     {/* <h2>Project 1</h2> */}
-                    <a target="_blank" href='https://www.linkedin.com/in/matthew-ev-walker/' >
-                        <img src={me1} height="300" width="425" />
+                    <a target="_blank" href='https://matthewevwalker.github.io/Password_Generator/' >
+                        <img src={password} height="300" width="425" />
                     </a>
 
 
