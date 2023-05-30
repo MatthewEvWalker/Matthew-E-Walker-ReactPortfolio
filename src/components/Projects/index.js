0eee00186df299a587ed47e9362ee372a6e01ccc
Matthew Walker <matthewevwalker@gmail.com>
2023-05-29 21:15:24 -0500

    added the text and it looks good now. The animation works as well. Now I need to complete the About me section.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,11 +9,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-
 const Project = () => {
     const [letterClass, setLetterClass] = useState('text-animate') 
-
-
 
     useEffect(() => {
         setTimeout(() => {
@@ -32,26 +29,48 @@ const Project = () => {
                     />
                 </h1>
 
-                <p>
-                The following are some of my projects. Click on the images to view the deployed application or the GitHub icons in the bottom left of the projects to view the code on GitHub.
+                <p className='top-p'>
+                    The following are some of my projects. Click on the images to view the deployed application or the GitHub icons in the bottom left of the projects to view the code on GitHub.
+                </p>
 
+                <h2 className='header-crypto'>
+                    Whatacrypto
+                </h2>
+
+                <p>
+                    WhataCrypto is a cryptocurrency game that allows people to make decisions on how and when to buy and sell crypto. WhataCrypto promotes strategic thinking and gives people opportunities to learn in a low stakes environment.
 
                 </p>
 
-                <h2>
-                    whatacrypto
+                <h2 className='header-slither'>
+                    SlitherAi
                 </h2>
 
+                <p>
+                    This is a single page MERN application. It illustrates an example of a Deep Q Network (DQN) Reinforced Learning. It automates the snake game, by implementing qTables to enable Reinforced Learning. The snake learns to play the game by itself and gets better over time.
+                </p>
+
+                <h2 className='header-scheduler'>
+                    Work Day scheduler
+                </h2>
+
+                <p>
+                    This is a simple static website that you can use to schedule your work day. It uses moment.js to display the current date and time. It also uses local storage to save your schedule.
+                </p>
                 
+                <h2 className='header-password'>
+                    Password Generator
+                </h2>
+
+                <p>
+                    A beginner friendly website password generator that allows you to generate a password between 8 and 128 characters. You can choose to include uppercase, lowercase, numbers, and special characters.
+                </p>
             </div>
 
 
             <div className="project-area">
                     
                     <div className="project-1">
-                        {/* <h2>Project 1</h2> */}
-
-
                         <a target="_blank" href='https://matthewevwalker.github.io/whatacrypto-crypto-game/' >
                             <img src={crypto} height="300" width="425" />
                         </a>
@@ -68,9 +87,6 @@ const Project = () => {
 
 
                     <div className="project-2" >
-                        {/* <h2>Project 1</h2> */}
-
-
                         <a target="_blank" href='https://github.com/MatthewEvWalker/Slither-AI' >
                             <img src={slither} height="300" width="425" />
                         </a>
@@ -86,7 +102,6 @@ const Project = () => {
                     </div>
 
                     <div className="project-3">
-                        {/* <h2>Project 1</h2> */}
                         <a target="_blank" href='https://matthewevwalker.github.io/work-scheduler-5/' >
                             <img src={workDay} height="300" width="425" />
                         </a>
@@ -102,7 +117,6 @@ const Project = () => {
                     </div>
 
                     <div className="project-4">
-                        {/* <h2>Project 1</h2> */}
                         <a target="_blank" href='https://matthewevwalker.github.io/Password_Generator/' >
                             <img src={password} height="300" width="425" />
                         </a>
